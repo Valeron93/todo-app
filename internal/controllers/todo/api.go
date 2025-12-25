ececc214848a603ec58f356f5915aad3367a7721
Valeron93 <valera1800@gmail.com>
2025-12-25 17:23:51 +0100

    remove unused IndexTemplateData struct

--- internal/controllers/todo/api.go
+++ internal/controllers/todo/api.go
@@ -8,11 +8,6 @@ type TodoController struct {
 	todoRepo model.TodoRepo
 }
 
-type IndexTemplateData struct {
-	Todos []model.Todo
-	User  model.User
-}
-
 func New(todoRepo model.TodoRepo) *TodoController {
 	return &TodoController{
 		todoRepo: todoRepo,
